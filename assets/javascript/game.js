caab1e272f5e6b4b5a30ad4d2e052067f80965fe
xtianares <xtianares@yahoo.com>
2018-12-08 02:31:26 -0500

    added function to display correct letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,14 @@ let wins = 0,
     newGame = true,
     guessedLetters = [];
 
+let letterDivs = document.querySelectorAll('.letter-display div');
+
 // function to select and display current word
 function currentWord() {
     theWord = wordList[Math.floor(Math.random() * wordList.length)];
-	console.log(theWord);
+    //theWord = "monster";
+    theWord = theWord.toLowerCase();
+	console.log("The Word is: " + theWord);
     for(var i = 0; i < theWord.length; i++) {
     	charCount += '<div class="letter"></div>';
     }
@@ -22,22 +26,13 @@ function gameInit() {
     document.querySelector('#game-wrap').style.display = 'flex';
     document.querySelector('.remaining-guesses').innerHTML = guesses;
     document.querySelector('.win-count').innerHTML = wins;
-    let letterDivs = document.querySelectorAll('.letter-display div');
+    // adding guessed class to the selected letter
     for(var i = 0; i < letterDivs.length; i++) {
         letterDivs[i].classList.remove("guessed");
     }
+    newGame = false;
 }
-function reset() {
-    wins = 0,
-    guesses = 7,
-    theWord = "",
-    charCount = "",
-    newGame = true,
-    guessedLetters = [];
-    gameInit();
-}
-function selectedLetter(letter) {
-    let letterDivs = document.querySelectorAll('.letter-display div');
+function checkLetter(letter) {
     if (guessedLetters.indexOf(letter) < 0) {
         guessedLetters.push(letter);
         for(var i = 0; i < letterDivs.length; i++) {
@@ -45,29 +40,49 @@ function selectedLetter(letter) {
                 letterDivs[i].classList.add("guessed");
             }
         }
-        guesses -= 1;
-    }
-    document.querySelector('.remaining-guesses').innerHTML = guesses;
-    console.log(guessedLetters);
-    if (guesses === 0) {
-        alert("YOU LOSE!");
-        reset();
+        let currentWordDivs = document.querySelectorAll('.current-word div');
+        for(var i = 0; i < theWord.length; i++) {
+            if (theWord.charAt(i) === letter) {
+                currentWordDivs[i].innerHTML = letter;
+                console.log("Letter in word: " + theWord.charAt(i));
+            }
+        }
+        if (theWord.indexOf(letter) < 0) {
+            guesses = guesses - 1;
+        }
+        document.querySelector('.remaining-guesses').innerHTML = guesses;
+        //console.log(guessedLetters);
+        if (guesses === 0) {
+            alert("YOU LOSE!");
+            reset();
+        }
     }
 }
+function reset() {
+    wins = 0,
+    guesses = 10,
+    theWord = "",
+    charCount = "",
+    newGame = true,
+    guessedLetters = [];
+    gameInit();
+}
+
 
 document.onkeyup = function(event) {
     if (newGame) {
         gameInit();
     }
-    newGame = false;
-    // Check if letter key
-    let charCode = event.keyCode;
-    let letter = String.fromCharCode(charCode).toLowerCase();
-    if (/[a-zA-Z]/i.test(letter)) {
- 		//console.log("Letter: " + letter);
-        selectedLetter(letter);
-    }
     else {
-        alert("Press a letter!");
+        // Check if letter key
+        let charCode = event.keyCode;
+        let letter = String.fromCharCode(charCode).toLowerCase();
+        if (/[a-zA-Z]/i.test(letter)) {
+     		//console.log("Letter: " + letter);
+            checkLetter(letter);
+        }
+        else {
+            alert("Press a letter!");
+        }
     }
 }
